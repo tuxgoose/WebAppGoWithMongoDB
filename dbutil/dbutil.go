4ff6139dab1a00a52707397b769fd63f6f5ddd06
Kelvin S. do Prado <kelvinpfw@hotmail.com>
2016-08-12 22:19:51 -0300

    Changes in the dbutil package

--- dbutil/dbutil.go
+++ dbutil/dbutil.go
@@ -8,6 +8,7 @@ import (
     "github.com/kelvins/webAppGoWithMongoDB/datastructure"
 )
 
+// Stablish a connection
 func Connect(url string) *mgo.Session {
     session, err := mgo.Dial(url)
     if err != nil {
@@ -16,30 +17,32 @@ func Connect(url string) *mgo.Session {
     return session
 }
 
-func Insert(c* mgo.Collection, p datastructure.Content) bool {
+// Insert the object into the collection
+func Insert(c* mgo.Collection, p datastructure.Post) bool {
     err := c.Insert(&p)
     if err != nil {
-        log.Println("Could not insert the object")
-        log.Println(err)
+        log.Println("Error in the Insert function: ", err)
         return false
     }
     return true
 }
 
-func FindAll(c* mgo.Collection) []datastructure.Content {
-    var results []datastructure.Content
+// Get all the data from the collection
+func FindAll(c* mgo.Collection) []datastructure.Post {
+    var results []datastructure.Post
     err := c.Find(nil).All(&results)
     if err != nil {
-        log.Println("Empty")
+        log.Println("Error ni the FindAll function: ", err)
     }
     return results
 }
 
-func Find(c* mgo.Collection, query bson.M) datastructure.Content {
-    var result datastructure.Content
+// Search for an element
+func Find(c* mgo.Collection, query bson.M) datastructure.Post {
+    var result datastructure.Post
     err := c.Find(query).One(&result)
     if err != nil {
-        log.Println("Could not find the element")
+        log.Println("Error in the Find function: ", err)
     }
     return result
 }
